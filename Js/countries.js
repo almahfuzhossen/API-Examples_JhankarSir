6224f2d63f0ad0ece8b6c57f587b08ff4b7592ef
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-16 22:13:44 +0600

    country

--- Js/countries.js
+++ Js/countries.js
@@ -24,3 +24,28 @@ const displayCountries = countries => { */
         countriesDiv.appendChild(div);
     })
 } */
+
+const loadCountries = () => {
+    fetch ('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+}
+loadCountries();
+
+const displayCountries = counties => {
+    // for (const country of counties) {
+    //     console.log(country);
+    // }
+    const countriesDiv = document.getElementById('country-detail');
+    counties.forEach(country => {
+        // console.log(country);
+        const div = document.createElement('div');
+        div.classList.add('country')
+        div.innerHTML = `
+            <h3>${country.region}</h3>
+            <p>${country.capital}</p>
+            <button onclick="loadCountryByName('${country.common}')">Details</button>
+        `;
+        countriesDiv.appendChild(div);
+    });
+}
